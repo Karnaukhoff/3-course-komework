e4014a5d0527986d1472fad66b83126d2f74b3c6
karnaukhoff <n4echduf@gmail.com>
2023-08-21 19:04:10 +1000

    Добавлен механизм переключения

---
+++ index.js
@@ -0,0 +1,16 @@
+import { getPageChoiceLevel, getPageGame } from "./pages.js";
+
+getPageChoiceLevel();
+
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", () => {
+    if (document.getElementById("radio1").checked) {
+        getPageGame(document.getElementById("radio1").value);
+    } else if (document.getElementById("radio2").checked) {
+        getPageGame(document.getElementById("radio2").value);
+    } else if (document.getElementById("radio3").checked) {
+        getPageGame(document.getElementById("radio3").value);
+    } else {
+        alert("Выберите уровень")
+    }
+})
